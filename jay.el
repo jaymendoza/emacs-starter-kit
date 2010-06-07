9d3e33e3f2817af195bf8147b38bc6bdba3a568b
Jay Mendoza <jayzer@gmail.com>
2010-06-06 23:46:55 -0500

    Add twittering-mode

--- jay.el
+++ jay.el
@@ -1,4 +1,10 @@
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
 (require 'jay/meta)
 (require 'jay/gui)
 (require 'jay/keyboard)
 (require 'jay/swank-clojure)
+
+(load (concat dotfiles-dir "vendor/twittering-mode/twittering-mode.el"))
+(require 'twittering-mode)
+(require 'jay/twitter-account)
